bfe2320ea5682965e81739c6312275dd6c61ad19
wyrenside <haciyev004@icloud.com>
2025-03-13 22:48:34 +0400

    Third task

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -13,18 +13,29 @@
             //    Console.WriteLine(txet);
             //}
 
-            int month = Convert.ToInt32(Console.ReadLine());
-            int year = 0;
-            int months = 0;
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //int year = 0;
+            //int months = 0;
+            //{
+            //    year = (month / 12);
+
+            //    if (month % 12 < 12)
+            //        months = month % 12;
+
+            //    Console.WriteLine(year + " " + "Year(s)");
+            //    Console.WriteLine(months + " " + "Month(s)");
+            //}
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int factorialofnum = 1;
             {
-                year = (month / 12);
+                for (i = 1; i <= num; i++)
+                    factorialofnum *= i;
 
-                if (month % 12 < 12)
-                    months = month % 12;
-
-                Console.WriteLine(year + " " + "Year(s)");
-                Console.WriteLine(months + " " + "Month(s)");
+                Console.WriteLine("Factorial of num is equal to" + " " + factorialofnum);
             }
+
 
         }
     }
