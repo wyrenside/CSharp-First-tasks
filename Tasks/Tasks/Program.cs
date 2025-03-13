290a4f1529ee4bbc97a0a3fa1815130de2c04b57
wyrenside <haciyev004@icloud.com>
2025-03-13 22:46:13 +0400

    Second task

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -4,14 +4,28 @@
     {
         static void Main(string[] args)
         {
-            string text = Console.ReadLine();
-            string txet = "";
-            {
-                for (int i = text.Length - 1; i >= 0; i--)
-                    txet += text[i];
+            //string text = Console.ReadLine();
+            //string txet = "";
+            //{
+            //    for (int i = text.Length - 1; i >= 0; i--)
+            //        txet += text[i];
 
-                Console.WriteLine(txet);
+            //    Console.WriteLine(txet);
+            //}
+
+            int month = Convert.ToInt32(Console.ReadLine());
+            int year = 0;
+            int months = 0;
+            {
+                year = (month / 12);
+
+                if (month % 12 < 12)
+                    months = month % 12;
+
+                Console.WriteLine(year + " " + "Year(s)");
+                Console.WriteLine(months + " " + "Month(s)");
             }
+
         }
     }
 }
--- Tasks/Tasks/obj/Debug/net8.0/Tasks.AssemblyInfo.cs
+++ Tasks/Tasks/obj/Debug/net8.0/Tasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7908c11db9b6fd28572a15a241763873571ab7bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
