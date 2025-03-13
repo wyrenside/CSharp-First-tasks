7908c11db9b6fd28572a15a241763873571ab7bf
wyrenside <haciyev004@icloud.com>
2025-03-13 22:41:23 +0400

    First task

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string txet = "";
+            {
+                for (int i = text.Length - 1; i >= 0; i--)
+                    txet += text[i];
+
+                Console.WriteLine(txet);
+            }
+        }
+    }
+}
